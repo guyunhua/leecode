307d7d43d93badac908bf101b5ab515e1b0078a8
ZTGAME\guyunhua <guyunhua@ztgame.com>
2020-09-14 11:43:02 +0800

    94

---
+++ leecode/leecode/leecode94.h
@@ -0,0 +1,54 @@
+#pragma once
+#include<vector>
+#include<stack>
+
+using namespace std;
+//Definition for a binary tree node.
+struct TreeNode {
+   int val;
+   TreeNode* left;
+   TreeNode* right;
+   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    
+};
+class Solution94 {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        ret.clear();
+        insert(root);
+        return ret;
+    }
+
+    //µü´ú·¨
+    vector<int>  inorderTraversaldd(TreeNode* root) {
+        vector<int> ret1;
+        stack<TreeNode*> stack;
+        while (root != nullptr || !stack.empty()) {
+            while (root != nullptr)
+            {
+                stack.push(root);
+                root = root->left;
+            }
+
+            root = stack.top();
+            stack.pop();
+            ret1.push_back(root->val);
+            root = root->right;
+        }
+        return ret;
+    }
+
+private:
+    void insert(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+
+        insert(root->left);
+        ret.push_back(root->val);
+        insert(root->right);
+
+    }
+    
+    vector<int> ret;
+};
